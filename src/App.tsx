90ecc3eab5d1187202c1141885c86f26fe33c2fc
T-Terra <gabst138@gmail.com>
2024-09-17 19:16:36 -0300

    feat: Buttons add

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import Oficial, { Comp2, Comp3 } from "./components/Mult";
 import Primeiro from "./components/Primeiro";
 import RandomValue from "./components/Random"
 import Titulo from "./components/Titulo"
+import BotaoExc from "./components/Botao"
 
 export default () => (
     <SafeAreaView style={Style.App}>
-        <Titulo principal="Cadastro produto" secundario="Tela de cadastro do produto"/>
+        <BotaoExc/>
         {/*
+        <Titulo principal="Cadastro produto" secundario="Tela de cadastro do produto"/>
         <RandomValue min={1} max={60}/>
         <RandomValue min={1} max={60}/>
         <MinMax min={3} max={20}/>
